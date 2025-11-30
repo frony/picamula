0f36b95e06a867bb79a0e16486ed3e2087ae5360
Marcos Frony <mfrony@yahoo.com>
2025-11-29 23:20:53 -0800

    Update Content Security Policy (CSP)

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -6,6 +6,29 @@ const nextConfig = {
     domains: ['localhost'],
   },
   transpilePackages: ['@junta-tribo/shared'],
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
+              "style-src 'self' 'unsafe-inline'",
+              "img-src 'self' data: blob:",
+              "media-src 'self' blob:",
+              "connect-src 'self' https://www.juntatribo.com https://juntatribo.com https://unpkg.com",
+              "worker-src 'self' blob:",
+              "child-src 'self' blob:",
+              "font-src 'self' data:",
+            ].join('; '),
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
