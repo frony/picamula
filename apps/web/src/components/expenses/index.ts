5a11a42cb09645b846557e55aa863907763d6837
Marcos Frony <mfrony@yahoo.com>
2025-12-04 17:52:56 -0800

    Removed TripExpensesPage exports from index

--- apps/web/src/components/expenses/index.ts
+++ apps/web/src/components/expenses/index.ts
@@ -1,5 +1,4 @@
 export { ExpenseForm } from './ExpenseForm';
 export { ExpenseList } from './ExpenseList';
 export { ExpensesSummary } from './ExpensesSummary';
-export { TripExpensesPage } from './TripExpensesPage';
 export { TripExpensesSection } from './TripExpensesSection';
